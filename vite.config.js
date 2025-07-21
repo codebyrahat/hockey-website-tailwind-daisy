700f36a97326f98421f8b8a7807818835e82d208
Rahat.dev <rahatalam.webdev@gmail.com>
2025-07-22 00:05:29 +0600

    learning complete how to set vite preview in github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  base: "/hockey-website-tailwind-daisy/",
   plugins: [tailwindcss()],
 });
